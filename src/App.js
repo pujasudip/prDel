7631e0c9d5c37fb07d34a9e0bcd4fd448ee02dd4
Sudip Baral <pujasudip@gmail.com>
2020-06-02 12:11:34 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './user-list/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Profile from './user-list/Profile';
+import ProfileEdit from './user-list/ProfileEdit';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/profile" component={Profile} />
+            <Route path="/edit/:id" component={ProfileEdit} />
+        </Switch>
+      </Router>
     );
   }
 }
