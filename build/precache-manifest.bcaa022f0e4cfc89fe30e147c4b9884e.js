fcde0838f9def2b10bcf03ac6bfb5fe5452be054
Sudip Baral <pujasudip@gmail.com>
2020-06-03 08:47:44 -0700

    changes

---
+++ build/precache-manifest.bcaa022f0e4cfc89fe30e147c4b9884e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0769513d0e82f1a1ff9b",
+    "url": "/static/js/main.0769513d.chunk.js"
+  },
+  {
+    "revision": "5cbe9eb853ba089007b2",
+    "url": "/static/js/1.5cbe9eb8.chunk.js"
+  },
+  {
+    "revision": "0769513d0e82f1a1ff9b",
+    "url": "/static/css/main.d48290d7.chunk.css"
+  },
+  {
+    "revision": "5cbe9eb853ba089007b2",
+    "url": "/static/css/1.c2621adf.chunk.css"
+  },
+  {
+    "revision": "cf656cefeea1653f9dd366750cee92ce",
+    "url": "/index.html"
+  }
+];
